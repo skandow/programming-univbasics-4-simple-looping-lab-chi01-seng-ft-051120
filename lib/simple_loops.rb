5f717f55799b0018863814f1ff523abbf85ee423
Steven Kandow <kando1sr@gmail.com>
2020-03-30 16:50:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,13 @@ def loop_message_n_times(message, n)
     count += 1 
   end 
 end 
+
+def output_array(array)
+  message = ""
+  count += 0
+  while count < array.length do 
+    message += array[count]
+    count += 1 
+  end 
+  message 
+end 
